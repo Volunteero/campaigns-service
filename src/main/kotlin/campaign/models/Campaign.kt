e2e8ac046bd924bfd2dd77b7e5d4c2b6f4a39535
Alexandru Matei Prodrom <prodromalex@yahoo.com>
2018-05-13 19:58:21 +0200

    database manager implemented without exceptions handling

--- src/main/kotlin/campaign/models/Campaign.kt
+++ src/main/kotlin/campaign/models/Campaign.kt
@@ -1,13 +1,11 @@
 package campaign.models
 
-import org.bson.types.ObjectId
 import org.mongodb.morphia.annotations.Entity
 import org.mongodb.morphia.annotations.Id
 
 @Entity("campaign")
 class Campaign(var organizationId: String = "", var name: String = "",
                var description: String = "", var influencePoints: Int = 0) {
-
     @Id
     var id: String? = null
 }
